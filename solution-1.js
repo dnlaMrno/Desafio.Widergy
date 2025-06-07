746a767d2533d4c34d7baac7014320d84672cdae
dnlaMrno <danip2824@gmail.com>
2025-06-07 05:29:14 -0300

    [FIX] solucion 1

--- solution-1.js
+++ solution-1.js
@@ -1,23 +1,21 @@
 function FormatString(sentence) {
-  let result = '';
-
-  for (let i = 0; i < sentence.length; i++) {
-    const charCode = sentence.charCodeAt(i);
-
-    if (
-      (charCode >= 65 && charCode <= 90) || // A-Z
-      (charCode >= 97 && charCode <= 122) || // a-z
-      (charCode >= 48 && charCode <= 57) || // 0-9
-      charCode === 32 ||  // espacio
-      charCode === 45 ||  // guion medio
-      charCode === 95     // guion bajo
-    ) {
-      result += sentence[i];
+    let result = [];
+    for (let i = 0; i < sentence.length; i++) {
+        const code = sentence.charCodeAt(i);
+        if (
+            (code >= 65 && code <= 90) ||  // A-Z
+            (code >= 97 && code <= 122) || // a-z
+            (code >= 48 && code <= 57) ||  // 0-9
+            code === 32 ||  // espacio
+            code === 45 || // guion
+            code === 95   // guion bajo
+        ) {
+            result.push(sentence[i]);
+        }
     }
-  }
-
-  return result.length > 0 ? result : "ERROR";
+    return result.length ? result.join('') : "ERROR";
 }
+
 
 function validateText() {
   const inputText = document.getElementById('textInput');
